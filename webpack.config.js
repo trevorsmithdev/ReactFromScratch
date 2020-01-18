7ed6f6e10a93e6f5467c0fd90546ab2d74e3d5e7
trevorsmithdev <43939983+trevorsmithdev@users.noreply.github.com>
2020-01-18 14:56:45 -0600

    added eslint and prettier w/ some new scripts

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,34 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    "entry": "./src/index.js",
-    "output": {
-        path: path.resolve(__dirname, 'build'),
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: "./build",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\css$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader'
-                    }
-                ]
-            }
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './build'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({ template: './src/index.html'})
+      }
     ]
-}
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })]
+};
